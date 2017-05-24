51516ff12515b18a292d98ea20ad1ff210ef8b90
YonghuiDong <Dong@users-MacBook-Pro.local>
2017-05-24 23:44:15 +0300

    R script
    add parameter descriptions
    Add input variable check

--- R/Miso.R
+++ R/Miso.R
@@ -6,24 +6,45 @@
 
 
 # isotope differences, can be extended according to the feeding precusors
-H2 = (2.014102 - 1.007825)
-C13 = (13.003355 - 12)
-N15 = (15.000109 - 14.003074)
-O18 = (17.999160 - 15.994915)
-S34 = (33.967867 - 31.972071)
+iso_DB <- c(H2 = (2.014102 - 1.007825),
+            C13 = (13.003355 - 12),
+            N15 = (15.000109 - 14.003074),
+            O18 = (17.999160 - 15.994915),
+            S34 = (33.967867 - 31.972071),
+            0
+            )
 
-# dependency checking, and install required packges if needed
-if(!require(S4Vectors)) {
-  message("installing the 'S4Vectors' package")
-  source("https://bioconductor.org/biocLite.R")
-  biocLite("S4Vectors")
-}
-
-
-
+#' Filtering isotopically labeled analytes according to RT and mass differences
+#' @param iso1 the first labled atom in precusor ion
+#' @param n11 the maximum numbers of the first labled atoms expected in the labeled intermediates
+#' @param n12 the minimum numbers of the first labled atoms expected in the labeled intermediates
+#' @param iso2 the second labled atom in the same precusor ion, if exist. it is 0 by default
+#' @param n21 the maximum numbers of the second labled atoms expected in the labeled intermediates
+#' @param n22 the minimum numbers of the second labled atoms expected in the labeled intermediates
+#' @param exp.base the control group (fed with unlabled precusor)
+#' @param exp.iso isotope labeled group
+#' @param ppm mass tolarance
+#' @param rt.dif retention time tolarance
+#' @return results containing unlabled and their corresponding labled analytes, with their RT and labeling information
 
 dual.iso <- function(iso1, n11, n12,  iso2 = 0, n21 = 0, n22 = 0, exp.base,
                      exp.iso, ppm = 100, rt.dif = 0.2) {
+  # check the input variables
+  if (!is.element(iso1, iso_DB)) {
+    stop('The labeled atom "iso1" does not exist. The atom letter should be capitalized')
+  }
+
+  if (!is.element(iso2, iso_DB)) {
+    stop('The labeled atom "iso2" does not exist. The atom letter should be capitalized')
+  }
+
+  if (n11 < n12) {
+    stop('The argument "n11" must be no less than "n12"' )
+  }
+
+  if (n21 < n22) {
+    stop('The argument "n21" must be no less than "n22"' )
+  }
 
   # prepare the labelling pattern according to the feeding precusor. iso2 is 0 by default.
   pattern1 <- seq(n11, n12, by = -1)
@@ -75,7 +96,11 @@ dual.iso <- function(iso1, n11, n12,  iso2 = 0, n21 = 0, n22 = 0, exp.base,
 
 
 
-# full result
+#' Export full result
+#' @param iso.C result from the first labeled precusor from Exp. C
+#' @param iso.D result from the second labled precusor from Exp. D
+#' @return an csv file containing the all the possible combined results. Full list, but redundant
+#'
 fResult <- function(iso.C, iso.D) {
   require(S4Vectors)
   p <- as.matrix(findMatches(iso.C$B.mz, iso.D$B.mz))
@@ -92,7 +117,10 @@ fResult <- function(iso.C, iso.D) {
 
 
 
-# simple and dirty result
+#' simple but dirty result
+#' @param full_Result the full result
+#' @return reduced result
+#'
 rResult <- function(full_Result) {
   Result <- as.data.frame(full_Result)
   ## remove B, where both mz and RT are the same
