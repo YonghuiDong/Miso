50b6ae506b34958a6fb438d2d62c1035221c75c3
YonghuiDong <Dong@users-MacBook-Pro.local>
2018-01-29 23:45:58 +0200

    Change description

--- R/miso.R
+++ R/miso.R
@@ -23,7 +23,7 @@
 #' exp.base = iso.C[,1:2], exp.iso = exp.D)
 
 
-miso <- function(iso1, n11, n12,  iso2 = 'NO', n21 = 0, n22 = 0, exp.base,
+miso <- function(iso1, n11, n12, iso2 = 'NO', n21 = 0, n22 = 0, exp.base,
                      exp.iso, ppm = 30, rt.dif = 6) {
   # check the input variables
   cat("\n(1) Checking input parameters...");
