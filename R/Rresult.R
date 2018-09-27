46fb57ab216d7ef03fc5fe42600f2303809410cf
YonghuiDong <Dong@users-MacBook-Pro.local>
2018-09-27 18:43:57 +0300

    Update Rresult() function to remove natural isotopes

--- R/Rresult.R
+++ R/Rresult.R
@@ -1,7 +1,7 @@
 #' @title Reduced result list
 #' @description export reduced isotope labeled result list
 #' @param full_Result full result list
-#' @param cutint cutoff intensity. rows that any ion intensities below the cutoff value will removed
+#' @param cutint cutoff intensity.Rows that any ion intensities below the cutoff value will removed
 #' @return csv file with repeated results being removed
 #' @export
 #' @examples
@@ -17,20 +17,39 @@
 #' reduced_Result <- Rresult(full_Result)
 
 Rresult <- function(full_Result, cutint = 0) {
+
   Result <- as.data.frame(full_Result)
+
   #(1) remove note for "no visible binding for global variable"
-  Unlabel_mz <- Unlabel_rt <- Label_I_int <- Label_II_int <- NULL
+  Unlabel_mz <- Unlabel_rt <- Unlabel_int <- Label_I_mz <- Label_I_rt <- Label_I_int <-
+    Label_II_mz <- Label_II_rt <- Label_II_int <- NULL
+
   #(2) find base peaks for Label I and Label II molecules
   Result1 <- Result %>%
     group_by(Unlabel_mz, Unlabel_rt) %>%
     filter(Label_I_int == max(Label_I_int), Label_II_int == max(Label_II_int))
-  #(3) remove repeated rows that cannot be removed using '!duplicated()' function
+
+  #(3) find base peaks for unlabled molecules
+  ## Filter according to Labeled I
+  Result2 <- Result1 %>%
+    group_by(Label_I_mz, Label_I_rt) %>%
+    filter(Unlabel_int == max(Unlabel_int))
+  ## Filter according to Labeled II
+  Result3 <- Result2 %>%
+    group_by(Label_II_mz, Label_II_rt) %>%
+    filter(Unlabel_int == max(Unlabel_int))
+
+  #(4) remove repeated rows that cannot be removed using '!duplicated()' function
   # due to some formatting differences.
-  Result2 <- Result1[!duplicated(Result1[,c(1, 2, 3)]),]
-  #(4) sort data according to RT
-  Result3 <- Result2[order(Result2$Unlabel_rt), ]
-  Result4 <- Result3[Result3$Unlabel_int >= cutint & Result3$Label_I_int >= cutint
-                      & Result3$Label_II_int >= cutint, ]
-  return(Result4)
+  Result4 <- Result3[!duplicated(Result3[,c(1, 2, 3)]),]
+
+  #(5) sort data according to RT
+  Result5 <- Result4[order(Result4$Unlabel_rt), ]
+
+  #(6) Filter according to intensity cutoff value. This is used to remove those minor peaks
+  # which is difficult to identify
+  Result6 <- Result5[Result5$Unlabel_int >= cutint & Result5$Label_I_int >= cutint
+                      & Result5$Label_II_int >= cutint, ]
+  return(Result6)
 }
 
