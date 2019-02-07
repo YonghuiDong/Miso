a51ad29bf755eed4f0123e2dbc751358e0267e21
YonghuiDong <yonghui.dong@gmail.com>
2019-02-07 19:12:32 +0200

    Update Rresult.R

--- R/Rresult.R
+++ R/Rresult.R
@@ -1,55 +1,36 @@
 #' @title Reduced result list
 #' @description export reduced isotope labeled result list
 #' @param full_Result full result list
-#' @param cutint cutoff intensity.Rows that any ion intensities below the cutoff value will removed
 #' @return csv file with repeated results being removed
 #' @export
 #' @examples
-#' data(lcms)
-#' explist <- prefilter(lcms[1: 100, ]) # use a subset of lcms data as example
-#' exp.B <- explist$exp.B
-#' exp.C <- explist$exp.C
-#' exp.D <- explist$exp.D
-#' iso.C <- diso(iso1 = 'H2', n11 = 4, n12 = 3, exp.base = exp.B, exp.iso = exp.C)
-#' iso.D <- diso(iso1 = 'C13', n11 = 9, n12 = 6, iso2 = 'N15', n21 = 1, n22 = 0,
-#' exp.base = iso.C[,1:3], exp.iso = exp.D)
-#' full_Result <- Fresult(iso.C, iso.D)
-#' reduced_Result <- Rresult(full_Result)
+#' a = 1+1
 
-Rresult <- function(full_Result, cutint = 0) {
+Rresult <- function(full_Result) {
+##(1) set global variable
+ group_by <- UQ<- filter_at <- vars <- all_vars <- "%>%" <- NULL
+ . <- NULL
 
-  Result <- as.data.frame(full_Result)
+ ##(2) find base peaks for Labeled molecules
+  ## get colnames according to m/z, rt and intensity
+  mz_name <- colnames(full_Result)[grepl("mz", colnames(full_Result))]
+  rt_name <- colnames(full_Result)[grepl("rt", colnames(full_Result))]
+  int_name <- colnames(full_Result)[grepl("intensity", colnames(full_Result))]
 
-  #(1) remove note for "no visible binding for global variable"
-  Unlabel_mz <- Unlabel_rt <- Unlabel_int <- Label_I_mz <- Label_I_rt <- Label_I_int <-
-    Label_II_mz <- Label_II_rt <- Label_II_int <- NULL
+  ## search for base peaks
+  n_name <- length(mz_name)
+  i = 1
+  r_result <- vector("list", length = n_name + 1)
+  r_result[[1]] <- full_Result
+  while (i <= n_name) {
+      r_result[[i+1]] <- as.data.frame(r_result[i]) %>%
+        group_by(UQ(as.name(mz_name[i])), UQ(as.name(rt_name[i]))) %>%
+        filter_at(vars(UQ(int_name[-i])), all_vars(. == max(.)))
+      i = i + 1
+  }
 
-  #(2) find base peaks for Label I and Label II molecules
-  Result1 <- Result %>%
-    group_by(Unlabel_mz, Unlabel_rt) %>%
-    filter(Label_I_int == max(Label_I_int), Label_II_int == max(Label_II_int))
-
-  #(3) find base peaks for unlabled molecules
-  ## Filter according to Labeled I
-  Result2 <- Result1 %>%
-    group_by(Label_I_mz, Label_I_rt) %>%
-    filter(Unlabel_int == max(Unlabel_int))
-  ## Filter according to Labeled II
-  Result3 <- Result2 %>%
-    group_by(Label_II_mz, Label_II_rt) %>%
-    filter(Unlabel_int == max(Unlabel_int))
-
-  #(4) remove repeated rows that cannot be removed using '!duplicated()' function
-  # due to some formatting differences.
-  Result4 <- Result3[!duplicated(Result3[,c(1, 2, 3)]),]
-
-  #(5) sort data according to RT
-  Result5 <- Result4[order(Result4$Unlabel_rt), ]
-
-  #(6) Filter according to intensity cutoff value. This is used to remove those minor peaks
-  # which is difficult to identify
-  Result6 <- Result5[Result5$Unlabel_int >= cutint & Result5$Label_I_int >= cutint
-                      & Result5$Label_II_int >= cutint, ]
-  return(Result6)
+  ## get reduced peaklist
+  reduced_result <- as.data.frame(r_result[[n_name]])
+  return(reduced_result)
 }
 
