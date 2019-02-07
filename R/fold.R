c870002321be3f2b406642d8bc299d6c4c2ad5c2
YonghuiDong <yonghui.dong@gmail.com>
2019-02-07 19:11:12 +0200

    Update fold.R

--- R/fold.R
+++ R/fold.R
@@ -1,30 +1,35 @@
 #' @title fold change
 #' @description calculate fold change among different samples.
-#' @param x matrix data, type: matrix
-#' @param f groups
+#' @param x dataframe
 #' @importFrom utils combn
-#' @return a dataframe with fold changes
+#' @return a dataframe with mean values and fold changes
 #' @export
 #' @examples
 #' vars <- 1000
 #' samples <- 50
 #' groups <- 3
 #' dat <- replicate(vars, runif(n = samples))
-#' f <- rep_len(1:groups, samples) + 1
+#' f <- rep_len(1:groups, samples)
 #' f <- LETTERS[f]
-#' ret <- fold(dat, f)
+#' dat <- data.frame(dat, group = f)
+#' ret <- fold(dat)
 
- fold <- function(x, f){
-  f <- factor(f, levels = c("D", "C", "B"))
+ fold <- function(x){
+  f <- x$group
   i <- split(1:nrow(x), f)
-  mean_int <- sapply(i, function(i){colMeans(x[i,])})
+  ## rm group info in order to calculate colMeans 
+  x$group = NULL
+  mean_int <- sapply(i, function(i){colMeans(x[i, ])})
   x <- t(mean_int)
   j <- combn(levels(f), 2)
-  f_change <- x[j[1,],] / x[j[2,],]
+  f_change1 <- x[j[1,],] / x[j[2,],]
+  f_change2 <- x[j[2,],] / x[j[1,],]
   ## remove NaN in f_change Matrix
+  f_change <- rbind(f_change1, f_change2)
   f_change[is.nan(f_change)] <- 0
-  rownames(f_change) <- paste('F_', j[1,], j[2,], sep = '')
+  rownames(f_change) <- c(paste(j[1,], "_", j[2,], sep = ''), paste(j[2,], "_", j[1,], sep = '') )
   f_change <- as.data.frame(t(f_change))
-  ret <- cbind.data.frame(mean_int, f_change, F_CD = 1 / f_change$F_DC)
+  colnames(mean_int) <- paste("mean_", colnames(mean_int), sep = "")
+  ret <- cbind.data.frame(mean_int, f_change)
   ret
 }
