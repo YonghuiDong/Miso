b46a7c2bf28a2726f5a14d1d5666bb385f6007ce
YonghuiDong <yonghui.dong@gmail.com>
2019-02-07 20:39:26 +0200

    solve the problem in CMD check

--- R/Rresult.R
+++ R/Rresult.R
@@ -1,10 +1,20 @@
 #' @title Reduced result list
 #' @description export reduced isotope labeled result list
 #' @param full_Result full result list
+#' @import dplyr
 #' @return csv file with repeated results being removed
 #' @export
 #' @examples
-#' a = 1+1
+#' data(lcms)
+#' explist <- prefilter(lcms, subgroup = c("B", "C", "D"), unlabel = "B")
+#' exp.B <- explist$B
+#' exp.C <- explist$C
+#' exp.D <- explist$D
+#' iso.C <- diso(iso1 = 'H2', n11 = 4, n12 = 2, exp.base = exp.B, exp.iso = exp.C)
+#' iso.D <- diso(iso1 = 'C13', n11 = 9, n12 = 6, iso2 = 'N15', n21 = 1, n22 = 0,
+#' exp.base = iso.C[,1:3], exp.iso = exp.D)
+#' full_result <- Fresult(iso.C, iso.D)
+#' reduced_result <- Rresult(full_result)
 
 Rresult <- function(full_Result) {
 ##(1) set global variable
@@ -24,8 +34,8 @@ Rresult <- function(full_Result) {
   r_result[[1]] <- full_Result
   while (i <= n_name) {
       r_result[[i+1]] <- as.data.frame(r_result[i]) %>%
-        group_by(UQ(as.name(mz_name[i])), UQ(as.name(rt_name[i]))) %>%
-        filter_at(vars(UQ(int_name[-i])), all_vars(. == max(.)))
+      group_by(UQ(as.name(mz_name[i])), UQ(as.name(rt_name[i]))) %>%
+      filter_at(vars(UQ(int_name[-i])), all_vars(. == max(.)))
       i = i + 1
   }
 
