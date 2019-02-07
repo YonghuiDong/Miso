c801a33b2acfbdc8cd28487967b270bbf3cae405
YonghuiDong <yonghui.dong@gmail.com>
2019-02-07 19:09:51 +0200

    xcms set data is included

--- R/lcms.R
+++ R/lcms.R
@@ -1,38 +1,15 @@
-#' LC-MS peaklist
-#'
-#' A LC-MS dataset containing the analytes from 5 experimental groups
-#'  lcms. The variables are as follows:
-#'
-#' \itemize{
-#'   \item mz.
-#'   \item mzmin.
-#'   \item mzmax.
-#'   \item rt.
-#'   \item rtmin.
-#'   \item rtmax.
-#'   \item npeaks.
-#'   \item A.
-#'   \item B.
-#'   \item C.
-#'   \item D.
-#'   \item E.
-#'   \item X11Feb15_04n.
-#'   \item X11Feb15_05n.
-#'   \item X11Feb15_06n.
-#'   \item X11Feb15_07n.
-#'   \item X11Feb15_08n.
-#'   \item X11Feb15_09n.
-#'   \item X11Feb15_11n.
-#'   \item X11Feb15_12n.
-#'   \item X11Feb15_13n.
-#'   \item X11Feb15_14n.
-#' }
-#'
-#' @docType data
-#' @keywords datasets
+#' @title An S4 class to represent a LC-MS dataset
+#' @description An S4 class to represent a LC-MS dataset.
+#' @slot peaks peaks
+#' @slot groups groupd
+#' @slot filled filled
+#' @slot phenoData phenoData
+#' @slot rt retention time
+#' @slot polarity polsrity
+#' @slot mslevel MS level
+#' @slot scanrange scan range
 #' @name lcms
 #' @usage data(lcms)
-#' @format A data frame with 16133 rows and 22 variables
 #' @examples
 #' data(lcms)
 NULL
